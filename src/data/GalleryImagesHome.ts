dcfa2084d152f7ce40c22ff5b8095e779769bda8
Brunito2017 <brunomontupil0@gmail.com>
2025-08-30 22:13:16 -0400

    feat: add homeGalleryImages data and enhance Gallery component layout

---
+++ src/data/GalleryImagesHome.ts
@@ -0,0 +1,22 @@
+import type { Image } from '../types/index.ts';
+
+export const homeGalleryImages: Image[] = [
+  {
+    src: 'https://i.pinimg.com/736x/5c/00/94/5c00944af356978f6252091cacd73f79.jpg',
+    alt: 'Lifting',
+    title: 'Lifting de Pestañas',
+    description: 'Tratamientos anti-edad'
+  },
+  {
+    src: 'https://i.pinimg.com/736x/b3/68/9d/b3689da7904f56a5f8f0b4d1dc12192e.jpg',
+    alt: 'cejas',
+    title: 'Diseño de Cejas',
+    description: 'Cuidado profesional'
+  },
+  {
+    src: 'https://i.pinimg.com/736x/90/28/e8/9028e8932ed22531c685df7b6f563c51.jpg',
+    alt: 'manicure',
+    title: 'Diseño de Uñas',
+    description: 'Perfección en cada detalle'
+  },
+];
